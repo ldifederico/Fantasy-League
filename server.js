a036e9beb307d6fc22c052223a98ef3cd697723e
Patrick Abromeit <patrick.abromeit@gmail.com>
2019-11-11 10:39:40 -0500

    connected server and added first 2 end points

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
@@ -18,5 +23,5 @@ app.get("/team", function(req, res) {
 });
 
 module.exports = function(app) {
-    
+
 };
