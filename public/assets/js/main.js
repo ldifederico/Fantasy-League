0ceb3670a1c744f2c3badd9b50218dbb7c7a99bf
lmd <difedericolinda@gmail.com>
2019-12-07 14:32:09 -0500

    styled fixtures

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -122,7 +122,7 @@ async function loadFixtures(gameWeek) {
                     class: "placeBet"+i,
                     type: "text",
                     placeholder: "Bet Amount",
-                    style: "width: 40%; border-radius: 5px;"
+                    style: "width: 80%; border-radius: 5px;"
                 }).appendTo(".fixRow"+i);
                 for ([a, bet] of ["Home", "Away", "Draw"].entries()){
                     $("<button>").attr({
